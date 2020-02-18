1e47175bf69b1270c4f6f88b36e331849aa4c477
koekto93 <koekto93@mail.ru>
2020-02-18 14:14:35 +0300

    update 7.

--- 7_generic.ts
+++ 7_generic.ts
@@ -1,3 +1,5 @@
+//Джинерик - это такой тип, который подстаивается под данные, к примеру в массиве. 
+//Можно не указывать какой тип элементов в массиве хардкодом, T[] сам сделает это
 const arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5]
 const arrayOfStrings: Array<string> = ['Hello', 'Vladilen']
 
@@ -8,4 +10,15 @@ function reverse<T>(array: T[]): T[] {
 
 reverse(arrayOfNumbers)
 reverse(arrayOfStrings)
+
+=========
+//Пример использования хука и джинерика
+export interface ITodo {
+    title: String,
+    id: number
+    completed: boolean
+}
+
+//для обозначения джинерика в хуке
+const [todos, setTodos] = useState<ITodo[]>([]) 
 
